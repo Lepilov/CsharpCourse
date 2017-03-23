037db36c4afec9f0be0ab113d3b953ef44af69f7
DESKTOP-53B702I\Илья <Lepilov18@gmail.com>
2017-03-23 21:26:26 +0300

    Массив случайных чисел, сортировка методом пузырька

---
+++ HomeWork2/Program.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HomeWork2
+{
+    class Program
+    {
+        
+        static void ArrayOfRandomNumbers(int[] array)
+        {
+            Random randomNumbers = new Random();
+
+            for (int i = 0, n = array.Length; i < n; i++)
+            {
+                array[i] = randomNumbers.Next(-10, 10);
+            }
+
+            Console.Write("Массив случайных чисел : ");
+
+            foreach (int item in array)
+            {
+                Console.Write(item + " ");
+            }
+            Console.WriteLine();
+
+            BubbleSort(array);
+        }
+
+        static void BubbleSort(int[] array)
+        {
+            Console.Write("Сортирует массив методом пузырька: ");
+
+            for (int i = 0; i < array.Length; i++)
+            {
+                for (int j = i + 1; j < array.Length; j++)
+                {
+                    if (array[i] > array[j])
+                    {
+                        int temp = array[i];
+                        array[i] = array[j];
+                        array[j] = temp;
+                    }
+                }
+            }
+
+            foreach (int item in array)
+            {
+                Console.Write(item + " ");
+            }
+
+        }
+
+        static void Main(string[] args)
+        {
+            Console.Write("Введите число : ");
+
+            uint number;
+
+            if (!(uint.TryParse(Console.ReadLine(), out number)))
+            {
+                Console.Write("Ввели не число");
+                Console.Read();
+                return;
+            }
+
+            int[] array = new int[number];
+            ArrayOfRandomNumbers(array);
+
+            Console.Read();
+        }
+    }
+}
