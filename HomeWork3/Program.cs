96c8d640cd4dba51ad87df5245826edb2fcfe012
DESKTOP-53B702I\Илья <Lepilov18@gmail.com>
2017-03-27 23:25:59 +0300

    Палиндром 1. Создать проект (консольное приложение) с именем HomeWork3 2. Написать программу, которая: 2.1. Принимает от пользователя строку на консоль. 2.2. Определяет является ли строка полиндромом 2.3. Определяет сколько в ней слов 2.4. Получает строку, перевернутую относительно заданной 3. Логика работы с одной строкой должна быть инкапсулирована в классе 3.1. Класс имеет поле, сохраняющее строку 3.2. Класс имеет конструктор, принимающий строку и устанавливающий ее в поле 3.3. Класс имеет 3 метода, выполняющие задачи 2.2, 2.3, 2.4.

---
+++ HomeWork3/Program.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HomeWork3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Введите строку : ");
+            string inputString = Console.ReadLine();
+
+            Palindrome palindrome = new Palindrome(inputString);
+
+            if (palindrome.isPalindrome() == true)
+            {
+                Console.WriteLine("Строка является палиндромом");
+                Console.WriteLine("Количество слов в строке : {0}", palindrome.SumOfWordsInLine());
+                Console.WriteLine("Перевернутая строка : {0}", palindrome.InvertedString());
+            }
+            else
+            {
+                Console.WriteLine("Строка не является палиндромом");
+            }
+
+            Console.Read();
+        }
+    }
+
+    class Palindrome
+    {
+        private string palindrome;
+
+        public Palindrome(string palindrome)
+        {
+            this.palindrome = palindrome;
+        }
+
+        public bool isPalindrome()
+        {
+            string lineFirstText = this.palindrome.Replace(" ", "").ToLower();
+            char[] arr = lineFirstText.ToCharArray();
+            Array.Reverse(arr);
+            string lineSecondText = new string(arr);
+            if (lineFirstText == lineSecondText)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        public int SumOfWordsInLine()
+        {
+            string[] sumOfWordsInLine = this.palindrome.Split();
+            return sumOfWordsInLine.Length;
+        }
+
+        public string InvertedString ()
+        {
+            char[] invertedString = this.palindrome.ToCharArray();
+            Array.Reverse(invertedString);
+            return new string(invertedString);
+        }
+
+    }
+}
