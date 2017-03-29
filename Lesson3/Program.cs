4f127b894457edb30ced2675b43bc5463d208a96
DESKTOP-53B702I\Илья <Lepilov18@gmail.com>
2017-03-29 21:13:21 +0300

    Lesson3

---
+++ Lesson3/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lesson3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите строку : ");
+            string enteredString = Console.ReadLine();
+
+            StringHelper sHelper = new StringHelper(enteredString);
+            //sHelper.inputString = enteredString;
+
+            Console.WriteLine(sHelper.IsPolyndrome() ? "Является полиндромом" : "Не является полиндромом");
+            Console.WriteLine($"Количество слов : {sHelper.GetCountWords()}");
+            Console.WriteLine($"Перевернутая строка : {sHelper.GetInverseString()}");
+
+            Console.Read();
+        }
+    }
+}
---
+++ Lesson3/StringHelper.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lesson3
+{
+    class StringHelper
+    {
+        private string inputString;
+
+        public StringHelper(string inputString)
+        {
+            this.inputString = inputString;
+        }
+
+        //throw new NotImplementedException(); не реализован метод, код
+        public bool IsPolyndrome()
+        {
+            if (inputString == null)
+            {
+                return false;
+            }
+            return inputString == GetInverseString();
+        }
+
+        public uint GetCountWords()
+        {
+            List<char> separators = new List<char>();
+            foreach (char ch in inputString)
+            {
+                if (!char.IsLetterOrDigit(ch) && !separators.Contains(ch))
+                {
+                    separators.Add(ch);
+                }
+            }
+
+            string[] words = inputString.Split(separators.ToArray());
+
+            uint countWords = 0;
+            foreach (string word in words)
+            {
+                //string.Empty проверка на пустату
+                //IsNullOrEmpty проверка на пустоту строки
+                if (!string.IsNullOrEmpty(word))
+                {
+                    countWords++;
+                }
+            }
+
+            return countWords;
+        }
+
+        public string GetInverseString()
+        {
+            StringBuilder result = new StringBuilder();
+
+            for (int i = inputString.Length - 1; i >= 0; i--)
+            {
+                result.Append(inputString[i]);
+            }
+
+            return result.ToString();
+        }
+    }
+}
