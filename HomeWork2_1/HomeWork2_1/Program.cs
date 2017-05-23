f0d5a36c2f2576db49617f448b26bef5c4c2c55b
DESKTOP-53B702I\Илья <Lepilov18@gmail.com>
2017-05-23 23:21:00 +0300

    Задание 1 1. Надо написать любой O(n^2) метод 2. написать парсер выражений с учетом приоритета операций и поддержку чисел с плавающей запятой.

---
+++ HomeWork2_1/HomeWork2_1/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace HomeWork2_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Введите пример : ");
+            string example = Console.ReadLine();
+            Console.Write("Ответ : ");
+            Console.Write(MathParser.Example(example.ToCharArray()).ToString());
+
+            //QuadraticAlgorithmExecutionTime q = new QuadraticAlgorithmExecutionTime();
+            //q.LeadTime(2, 2);
+
+            Console.Read();
+        }
+    }
+}
---
+++ HomeWork2_1/HomeWork2_1/QuadraticAlgorithmExecutionTime.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace HomeWork2_1
+{
+    class QuadraticAlgorithmExecutionTime
+    {
+        //O(n^2)
+        public void LeadTime(int num1, int num2)
+        {
+            int sum = 0;
+
+            for (int i = 0; i < num1; i++)
+            {
+                for (int j = 0; j < num2; j++)
+                {
+                    sum++;
+                }
+            }
+
+            Console.WriteLine("sum = {0}", sum);
+        }
+    }
+}
