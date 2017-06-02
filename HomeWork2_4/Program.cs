e7a64fd331f26af111a8585dbb3a7487cc58e551
DESKTOP-53B702I\Илья <Lepilov18@gmail.com>
2017-06-03 00:41:38 +0300

    написать extension метод  для date который определяет сколько лет прошло с указанной даты написать extension метод для IEnumerabale<Date> что конвертирует дату в строку

---
+++ HomeWork2_4/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HomeWork2_4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var date = DateTime.Parse("1990,04,15").Age();
+            Console.WriteLine(date);
+
+            var date2 = new DateTime(1945,5,9).Age();
+            Console.WriteLine(date2);
+            Console.WriteLine();
+
+            List<DateTime> dateList = new List<DateTime>();
+            dateList.Add(new DateTime(2001,5,20));
+            dateList.Add(new DateTime(1945,5,9));
+            foreach (var dates in dateList.DateString())
+            {
+                Console.WriteLine(dates);
+            }
+        }        
+    }
+
+    static class M
+    {
+        public static int Age(this DateTime date)
+        {
+            return DateTime.Today.Year - date.Year;
+        }
+
+        public static IEnumerable<string> DateString(this IEnumerable<DateTime> dates)
+        {
+            return dates.Select(date=>($"{date.Day}.{date.Month}.{date.Year}"));
+        }
+    }
+}
+    
