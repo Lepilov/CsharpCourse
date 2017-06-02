748bcc403409ed08aff05480c9182bde4000ac64
DESKTOP-53B702I\Илья <Lepilov18@gmail.com>
2017-06-03 01:27:36 +0300

    Парсер с исключениями

---
+++ HomeWork2_2_2/HomeWork2_2_2/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HomeWork2_2_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Введите пример : ");
+            try
+            {
+                string example = Console.ReadLine();
+                if (example == String.Empty)
+                {
+                    throw new FormatException();
+                }
+                else if (example == ".")
+                {
+                    throw new ArgumentException();
+                }
+                Console.Write("Ответ : ");
+                Console.Write(MathParser.Example(example.ToCharArray()).ToString());
+            }
+            catch(FormatException)
+            {
+                Console.WriteLine("Пустая строка");
+            }
+            catch (ArgumentException)
+            {
+                Console.WriteLine("Недопустимый символ \".\"");
+            }
+
+            Console.Read();
+        }
+    }
+}
