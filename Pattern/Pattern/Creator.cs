fba769a2eed1d3a851ea9c6cbbd2a28419098209
DESKTOP-53B702I\Илья <Lepilov18@gmail.com>
2017-06-16 21:42:56 +0300

    Фабричный метод

---
+++ Pattern/Pattern/Creator.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Pattern
+{
+    abstract class Creator
+    {
+        Shape shape;
+        public abstract Shape FactoryMethod();
+    }
+}
---
+++ Pattern/Pattern/TriangleCreator.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Pattern
+{
+    class TriangleCreator : Creator
+    {
+        public override Shape FactoryMethod()
+        {
+            return new RightTriangle();
+        }
+    }
+}
