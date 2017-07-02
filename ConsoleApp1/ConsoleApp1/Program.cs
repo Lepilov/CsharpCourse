fa4793586265e79c7ed848bc002d5f4489321291
DESKTOP-53B702I\Илья <Lepilov18@gmail.com>
2017-07-02 08:53:52 +0300

    Exam

---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string s1 = "one two free";
+            string s2 = "two";
+            string words = s1.Split(' ').OrderByDescending(x => x.Length).FirstOrDefault();
+            Console.WriteLine(Array.FindIndex(s1.Split(' '), x => x.Equals(s2)));
+        }
+    }
+}
